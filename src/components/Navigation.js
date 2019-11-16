1a7b512f1c5fd7c696b142e8e79f721522ee5a08
Maxwell Benton <maxwellbenton@gmail.com>
2019-11-16 17:05:53 -0500

    removes experiments

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,6 @@ function Navigation() {
     return(<nav>
         <NavLink className="navlink" to='/website/'>Home</NavLink>
         <NavLink className="navlink" to='/website/projects'>Projects</NavLink>
-        <NavLink className="navlink" to='/website/experiments'>Experiments</NavLink>
     </nav>)
 }
 export default Navigation;
