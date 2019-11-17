cf0ece0ddacb9241a90e19c8918da8fa3b0d207c
Maxwell Benton <maxwellbenton@gmail.com>
2019-11-17 15:15:48 -0500

    moves build contents to root folder

---
+++ precache-manifest.b89528bd8835c291cae4efc2768fcf1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b5deab62194c16ca697abe28b00011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcd8ac949666467e2d5e",
+    "url": "/static/css/main.86d5a4bf.chunk.css"
+  },
+  {
+    "revision": "7c17e1a3554d3d43403f",
+    "url": "/static/js/2.708881b5.chunk.js"
+  },
+  {
+    "revision": "bcd8ac949666467e2d5e",
+    "url": "/static/js/main.d92a680f.chunk.js"
+  },
+  {
+    "revision": "5d073984d2dc3a7ba29f",
+    "url": "/static/js/runtime~main.4497c250.js"
+  },
+  {
+    "revision": "6104966f61fb7e0bade6e9b9fc9bdb3a",
+    "url": "/static/media/profile.6104966f.png"
+  }
+]);
