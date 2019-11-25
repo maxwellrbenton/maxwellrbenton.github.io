b7d8bc183928ff13c6e9d4d92d7a3e055b17bb11
Maxwell Benton <maxwellbenton@gmail.com>
2019-11-24 21:47:56 -0500

    adjust projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import gameOfLife from "../assets/game-of-life.png";
+import { NavLink } from 'react-router-dom'
 
 const useStyles = makeStyles({
   card: {
@@ -22,8 +22,9 @@ const useStyles = makeStyles({
 function Projects() {
   const classes = useStyles();
   return(
-    <div>
-      <h3>Projects</h3>
+    <>
+    <div className="projectsPage">
+      <h3>PROJECTS</h3>
       <div className="projects">
         <Card className={classes.card}>
           <CardContent>
@@ -44,11 +45,16 @@ function Projects() {
               {'"a benevolent smile"'}
             </Typography>
             <br />
-            <a href="https://maxwellrbenton.github.io/react-game-of-life/"><Button size="small">View Project</Button></a>
+            <div><a href="https://maxwellrbenton.github.io/react-game-of-life/"><Button size="small">View Project</Button></a></div>
+            <div><a href="https://www.github.com/maxwellrbenton/react-game-of-life/"><Button size="small">View Repository</Button></a></div>
           </CardContent>
         </Card>
       </div>
     </div>
+    <section className="projectNav">
+      <NavLink className="navlink" to='/'>HOME</NavLink>
+    </section>
+    </>
   )
 
 }
