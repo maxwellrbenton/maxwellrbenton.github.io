35e76d9fce58fa336ecf178e3ef9b935f492920d
Maxwell Benton <maxwellbenton@gmail.com>
2019-11-24 21:46:53 -0500

    switches out old router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Home from './components/Home'
-import Experiments from './components/Experiments'
-import API from './components/API'
-import Navigation from './components/Navigation'
 import Projects from './components/Projects'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Navigation />
-      </header>
-      <main>
-        <Switch>
-          <Route exact path={process.env.PUBLIC_URL + '/'} component={Home}/>
-          <Route path={process.env.PUBLIC_URL + '/projects'} component={Projects}/>
-          <Redirect to='/' />
-        </Switch>
-      </main>
+    <div>
+      <Switch>
+        <Route exact path={process.env.PUBLIC_URL + '/'} component={Home}/>
+        <Route path={process.env.PUBLIC_URL + '/projects'} component={Projects}/>
+        <Redirect to='/' />
+      </Switch>
     </div>
   );
 }
